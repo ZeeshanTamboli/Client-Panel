f84cf3fc7894c75d13f3d6a09dad4c147bcd58d1
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2018-12-09 13:12:07 +0530

    Routes guarding

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Login from './components/auth/Login';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth';
+
 import './App.css';
 
 require('dotenv').config();
@@ -23,11 +25,31 @@ class App extends Component {
             <AppNavbar />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/client/add" component={AddClient} />
-                <Route exact path="/client/edit/:id" component={EditClient} />
-                <Route exact path="/client/:id" component={ClientDetails} />
-                <Route exact path="/login" component={Login} />
+                <Route
+                  exact
+                  path="/"
+                  component={UserIsAuthenticated(Dashboard)}
+                />
+                <Route
+                  exact
+                  path="/client/add"
+                  component={UserIsAuthenticated(AddClient)}
+                />
+                <Route
+                  exact
+                  path="/client/edit/:id"
+                  component={UserIsAuthenticated(EditClient)}
+                />
+                <Route
+                  exact
+                  path="/client/:id"
+                  component={UserIsAuthenticated(ClientDetails)}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  component={UserIsNotAuthenticated(Login)}
+                />
               </Switch>
             </div>
           </div>
