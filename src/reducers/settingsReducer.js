490bb30bab50edb5bdb8da8d7ccee9acf59ba82c
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2018-12-11 21:43:33 +0530

    Settings Reducer & Actions

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -13,6 +13,20 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case DISABLE_BALANCE_ON_ADD:
+      return {
+        ...state,
+        disableBalanceOnAdd: !state.disableBalanceOnAdd
+      };
+    case DISABLE_BALANCE_ON_EDIT:
+      return {
+        ...state,
+        disableBalanceOnEdit: !state.disableBalanceOnEdit
+      };
+    case ALLOW_REGISTRATION:
+      return {
+        ...state,
+        allowRegistration: !state.allowRegistration
+      };
 
     default:
       return state;
